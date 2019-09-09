4373af458aa8f5c43053412f2ebd3b3d9c23a044
Jeff Calder <jefcalder@gmail.com>
2019-09-09 18:57:35 -0500

    just doing classork

--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -1,24 +1,31 @@
 //Write the include statement for decisions.h here
-
+#include "decisions.h"
+#include <iostream>
 //Write include for capturing input from keyboard and displaying output to screen
-
+using std::cout;	using std::cin;
 
 //Write namespace using statements for cout and cin
 
 int main() 
 {
 	//create a double variable named hours
+	double hours;
 	//create a double variable named hourly_rate
-
+	double hourly_rate;
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
-
+	cout << "Enter hours:";
+	cin >> hours;
+	cout << "Enter hourly rate:";
+	cin >> hourly_rate;
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
+	double gross_salary;
+	gross_salary = gross_pay(hours, hourly_rate);
 
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
-
+	cout << "Gross pay:" << gross_salary;
 	return 0;
 }
 
