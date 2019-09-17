839f30bbe2a58c66ea98f958fd277d34f7477f76
Jeff Calder <jefcalder@gmail.com>
2019-09-16 19:34:31 -0500

    class work without testing. hope it doesnt break everything

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,12 +1,41 @@
 #include<iostream>
 #include "do_while.h"
-#include "switch.h"
 
 using std::cout;
 using std::cin;
 
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
+void prompt_user()
+{
+	auto user_choice = 'y';
+
+	do
+	{
+		//some more code for your program
+		cout << "Loop again y or n? ";
+		cin >> user_choice;
+	} 
+	while (user_choice =='y'|| user_choice == 'Y');//only time you use semicolon on while
+}
+
+string menu(int menu_option)
+{
+	switch (menu_option)
+	{
+	case 1:
+		return "Option 1";
+	case 2:
+		return "Option 2";
+	case 3:
+		return "Option 3";
+	case 4:
+		return "Option 4";
+	default:
+		return"Invalid";
+	}
+	return string();
+}
 
 
 //Write code for function run_menu that prompts  user for a 
@@ -16,4 +45,26 @@ Use the existing menu_option function from /example/02_module/03_switch
 folder.
 
 */
+void run_menu()
+{
+	auto user_choice = 'y';
+	auto choice = 0;
 
+	do
+	{
+		cout << "Enter Menu Option";
+		cin >> choice;
+
+		while (choice < 1 || choice > 4) 
+		{
+			cout << "enter menu option:";
+			cin >> choice;
+
+		}
+
+		cout << menu(choice) << "\n";
+
+		cout << "Continue y or n";
+		cin >> user_choice;
+	} while (user_choice == 'y' || user_choice == 'Y');
+}
