b3eddae7b194e07f2db35cdc4c3cf153402abc19
Jeff Calder <jefcalder@gmail.com>
2019-10-14 18:48:26 -0500

    checking in

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "atm.h"
 #include "bank_account.h"
+#include "customer.h"
 #include <vector>
 
 using std::vector;
@@ -9,23 +10,13 @@ using std::cin;
 
 int main()
 {
-	BankAccount a;
-	BankAccount b = a;
-	display(a);//class friend function
+	Customer customer;
+	customer.add_account(BankAccount(500));
+	customer.add_account(BankAccount(1500));
+	customer.add_account(BankAccount(5000));
 
-	//scan card and enter pin
-	//get customer records from db into a vector
-	vector<BankAccount> accounts{BankAccount(500), BankAccount(600), BankAccount(1000)};
+	cout << customer;
 
-
-
-
-	ATM atm(accounts[0]);
-	atm.deposit(100);
-	atm.withdraw(50);
-	atm.display_balance();
-	cin >> a;
-	cout << a; //overload operator
 
 	return 0;
 }
