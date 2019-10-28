006f14d8391b1eae2d98d0c362e032ae1d783e6a
Jeff Calder <jefcalder@gmail.com>
2019-10-28 18:09:00 -0500

    example of dereference in bank account main.

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -14,15 +14,18 @@ using std::reference_wrapper;
 
 int main()
 {
-	CheckingAccount c(1500);
-	SavingsAccount s(500);
+	int num = 5;
+	int & num_ref = num; //get the address of num
+	std::cout << "Address: " << &num_ref << std::endl; //output address
+	std::cout << "Value at above address: " << num_ref << std::endl; //output value at address of num
 
-	vector<reference_wrapper<BankAccount>> accounts{ c,s };
+	int * val_ = &num_ref; //point to the address of num
+	std::cout << "Address pointed to: " << val_ << std::endl; //get the address of num
+	std::cout << "Value pointed to: " << *val_ << std::endl; //get the value pointed to by val_
 
-	for (auto act : accounts)
-	{
-		cout << "Balance: " << act.get().get_balance() << "\n";
-	}
+	*val_ = 15;
+	std::cout << "Address pointed to: " << val_ << std::endl; //get the address of num
+	std::cout << "Value pointed to: " << *val_ << std::endl; //get the value pointed to by val_
 
 	return 0;
 }
