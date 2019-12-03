fc0f74002d8b0aa488893ce2b14d4913b7d389f5
Jeff Calder <jefcalder@gmail.com>
2019-12-02 19:05:22 -0600

    playing with arrays and pointers in class

--- src/examples/12_module/01_arrays_mem/arrays_mem.cpp
+++ src/examples/12_module/01_arrays_mem/arrays_mem.cpp
@@ -1,1 +1,46 @@
 //write code for for stack_array and display each element to screen
+#include "arrays_mem.h"
+#include<iostream>
+
+void stack_array()
+{
+	const int SIZE = 5;
+	int hours[SIZE] = { 5,10,30,20,7 };
+	hours[0] = 50;//access and modify
+	//interate an array
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cout << hours[i] << "\n";
+	}
+
+	std::cout << "\n";
+	//iterate with for ranged
+	for (auto hour : hours)// put a & before hour to modify the original
+	{
+		std::cout << hour << "\n";
+	}
+	//pointer to first hours array element
+	int* first_element = hours;
+	std::cout << "first_element: " << *first_element << "\n";
+	std::cout << "second_element: " << *(first_element + 1) << "\n";
+	*first_element++;
+	*first_element++;
+	std::cout << "third_element: " << *first_element++ << "\n";// gives same result with or without ++
+	*first_element--;
+	std::cout << "third_element: " << *first_element-- << "\n\n";
+
+	int* hours_ptr = hours;
+	//iterate array w a pointer forward
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cout << *hours_ptr << "\n";
+		hours_ptr++;
+	}
+
+	//iterate array pointer backwards
+	for (int i = 0; i < SIZE; i++)
+	{
+		hours_ptr--;
+		std::cout << *hours_ptr << "\n";
+	}
+}
--- src/examples/12_module/05_arrays_functions/main.cpp
+++ src/examples/12_module/05_arrays_functions/main.cpp
@@ -1,14 +1,32 @@
 #include "arr_functions.h"
+#include<iostream>
 
 int main() 
 {
 	const int SIZE = 5;
-
+	// hours is a pointer to the first array element/address
 	int hours[SIZE] = { 5, 10, 30, 20, 7 };
 
 	int* hours_ptr = hours;
 
 	iterate_array(hours_ptr, SIZE);
+	std::cout << "\n";
+
+	iterate_array(hours, SIZE);
+
+	int* numbers = new int[SIZE];//allocate memory
+	for (int i = 0; i < SIZE; i++)//initialize data
+	{
+		numbers[i] = i;
+	}
+	std::cout << "\n";
+	for (int i=0; i<SIZE; i++)//process
+	{
+		std::cout << numbers[i] << "\n";
+	}
+
+	delete[] numbers;//deallocate
+	numbers = nullptr;
 
 	return 0;
 }
